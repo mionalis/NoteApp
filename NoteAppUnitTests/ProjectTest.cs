27bff37306e91f9f75d699450fe1ddc4d0f8b960
mionalis <mionallis@gmail.com>
2024-04-08 17:26:32 +0700

    test(ProjectTest): написаны тесты для класса Project

--- NoteAppUnitTests/ProjectTest.cs
+++ NoteAppUnitTests/ProjectTest.cs
@@ -10,5 +10,27 @@ namespace NoteAppUnitTests
 {
 	internal class ProjectTest
 	{
+		[Test]
+		public void ProjectNotesSet_CorrectValue()
+		{
+			var project = new Project();
+			var testProject = new List<Note> { new() };
+
+			project.Notes = testProject;
+
+			Assert.That(testProject, Is.EqualTo(project.Notes));
+		}
+
+		[Test]
+		public void ProjectNotesGet_CorrectValue()
+		{
+			var expected = new List<Note> { new() };
+			var project = new Project();
+
+			project.Notes = expected;
+			var actual = project.Notes;
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
 	}
 }
