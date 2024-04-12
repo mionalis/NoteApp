ed12312ff80cdd38409aa3df2f5c25e3031cc036
mionalis <mionallis@gmail.com>
2024-04-12 13:59:56 +0700

    feature(Project): реализованы методы сортировки заметок

--- NoteApp/Project.cs
+++ NoteApp/Project.cs
@@ -9,4 +9,33 @@ public class Project
 	/// Список созданных заметок в приложении.
 	/// </summary>
 	public List<Note> Notes { get; set; } = new();
+
+	/// <summary>
+	/// Возвращает список заметок, отсортированных по убыванию
+	/// по времени редактирования.
+	/// </summary>
+	/// <param name="notes">Список заметок.</param>
+	/// <returns>Отсортированный список заметок.</returns>
+
+	public List<Note> GetSortedNotes(List<Note> notes)
+	{
+		return notes.OrderByDescending(note => note.LastModifiedTime).ToList();
+	}
+
+	/// <summary>
+	/// Возвращает список заметок одной категории, отсортированных по убыванию
+	/// по времени редактирования.
+	/// </summary>
+	/// <param name="notes">Список заметок.</param>
+	/// <param name="category">Выбранная категория.</param>
+	/// <returns>Отсортированный список заметок одной категории.</returns>
+	public List<Note> GetSortedNotes(List<Note> notes, NoteCategory category)
+	{
+		var filteredNotes = notes.Where(
+				note => note.Category == category).ToList();
+
+		var sortedFilteredNotes = GetSortedNotes(filteredNotes);
+
+		return sortedFilteredNotes;
+	}
 }
